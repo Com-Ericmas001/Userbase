2e610769c5398c74ef99d84af24fe9a3a39fc859
Éric Massé <Ericmas001@gmail.com>
2016-05-15 11:21:06 -0400

    Token property didn't serialize correctly in XML

--- Src/Com.Ericmas001.Userbase/Responses/Models/Token.cs
+++ Src/Com.Ericmas001.Userbase/Responses/Models/Token.cs
@@ -1,14 +1,18 @@
 ﻿using System;
 using System.Diagnostics.CodeAnalysis;
+using System.Xml.Serialization;
+// ReSharper disable All
 
 namespace Com.Ericmas001.Userbase.Responses.Models
 {
+    [XmlInclude(typeof(ConnectionToken))]
+    [XmlInclude(typeof(RecoveryToken))]
     public abstract class Token
     {
         protected abstract DateTime CalculateNextExpiration();
-        public Guid Id { get; }
+        public Guid Id { get; set; }
 
-        public DateTime ValidUntil { get; }
+        public DateTime ValidUntil { get; set; }
 
         protected Token(Guid id, DateTime validUntil)
         {
